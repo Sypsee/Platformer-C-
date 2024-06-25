1501387c218413516abe1d0e69ce5defbd5bbcc9
Naman Sharma <namansharma232009@gmail.com>
2024-06-25 14:04:09 +0530

    fix build.sh

--- build.sh
+++ build.sh
@@ -4,4 +4,4 @@ libs="-luser32 -lopengl32 -lgdi32"
 warnings="-Wno-writable-strings -Wno-format-security -Wno-deprecated-declarations"
 includes="-Ithird_party -Ithird_party/Include"
 
-clang++ $includes src/main.cpp -omain.exe $libs $warnings
+clang++ $includes -g src/main.cpp -omain.exe $libs $warnings
